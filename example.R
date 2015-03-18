c018edeedbcad8b6c27c55f4baa9f5759e5d2243
Cássio Jandir Pagnoncelli <cassiopagnoncelli@gmail.com>
2015-03-18 08:19:44 -0300

    moving average crossover example

--- example.R
+++ example.R
@@ -3,11 +3,13 @@ source('event-profiler.R', local=.GlobalEnv)
 source('reporting.R', local=.GlobalEnv)
 source('symbols.R', local=.GlobalEnv)
 
-#options(error = recover)
+options(error = recover)
 
 #
 # Expert advisor.
 #
+library('zoo')
+
 etl <- function() {
   instr <- 'petrobras'
   assign(instr, loadSymbol(instr), envir=.GlobalEnv)
@@ -16,15 +18,31 @@ etl <- function() {
 }
 
 vectorized <- function() {
-  time_index <- 100
+  n <- nrow(all_series)
+  
+  ma5 <- c(rep(0, 4), rollapply(all_series, 5, mean))
+  ma14 <- c(rep(0, 13), rollapply(all_series, 14, mean))
+  ma80 <- c(rep(0, 79), rollapply(all_series, 80, mean))
+  ma5d <- c(0, ma5[-n])
+  ma14d <- c(0, ma14[-n])
+  
+  signal_fast_crossover <- ma5 > ma14 & ma5d < ma14d
+  fast_is_up <- ma14 > ma80
+  
+  sd5 <- c(rep(0, 4), rollapply(all_series, 5, sd))
+  upper <- ma5 + 2*sd5
+  lower <- ma5 - 2*sd5
+  bollinger_ok <- all_series - lower > 0.2*(upper - lower)
   
   # Globally register only the series to be used.
-  eval({
-    
-  }, envir=.GlobalEnv)
+  assign('buy_signal', signal_fast_crossover & fast_is_up & bollinger_ok,
+         envir=.GlobalEnv)
+  
+  time_index <- 85
   
   # Return the starting time index.
-  ifelse(exists(as.character(substitute(time_index))), 1, time_index)
+  #ifelse(exists(as.character(substitute(time_index))), time_index, 1)
+  time_index
 }
 
 beginEA <- function() {
@@ -32,10 +50,10 @@ beginEA <- function() {
 }
 
 tickEA <- function() {
-  if (runif(1) < 0.025)
+  if (runif(1) < 0.02)
     closePosition()
   
-  if (runif(1) < 0.02)
+  if (buy_signal[epoch])
     buy()
 }
 
@@ -47,7 +65,7 @@ endEA <- function() {
 # Simulation.
 #
 runExpertAdvisor(etl, vectorized, beginEA, tickEA, endEA,
-                 list(deposit=10000, journaling=F))
+                 list(deposit=10000, journaling=FALSE))
 
 runEventProfiler()
 
