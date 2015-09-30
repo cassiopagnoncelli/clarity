e9b610c7005cec124d489d2fdb8e0ae57df7ed29
Cássio Jandir Pagnoncelli <cassiopagnoncelli@gmail.com>
2015-09-30 01:02:23 -0300

    rubbish

--- snippets/backward-difference.R
+++ snippets/backward-difference.R
@@ -3,6 +3,11 @@
 # https://en.wikipedia.org/wiki/Finite_difference
 # https://en.wikipedia.org/wiki/Five-point_stencil
 # http://stackoverflow.com/questions/14082525/how-to-calculate-first-derivative-of-time-series
+# http://www.geometrictools.com/Documentation/FiniteDifferences.pdf
+# http://ocw.mit.edu/courses/mechanical-engineering/2-29-numerical-fluid-mechanics-fall-2011/lecture-notes/MIT2_29F11_lect_12.pdf
+# http://www.uio.no/studier/emner/matnat/ifi/INF2340/v05/foiler/sim01.pdf
+# http://stats.stackexchange.com/questions/99160/kalman-filtering-smoothing-and-parameter-estimation-for-state-space-models-in-r
+# http://www.bearcave.com/finance/random_r_hacks/kalman_smooth.html
 
 
 # Re-code for h > 1.
