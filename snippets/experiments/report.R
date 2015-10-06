384a62501824debca692f400bbfee646750c7653
Cássio Jandir Pagnoncelli <cassiopagnoncelli@gmail.com>
2015-10-06 06:02:33 -0300

    add macro vars

--- snippets/experiments/report.R
+++ snippets/experiments/report.R
@@ -1,12 +1,14 @@
 source('snippets/experiments/models.R', local=.GlobalEnv)
 
 # Save variables
-save(
-  predictions,
-  y_test,
-  file='snippets/experiments/show.RData')
+save(predictions, y_test, file='snippets/experiments/show.RData')
 
 # Add macroeconomic variables: 
-#   inflation, unemployment, interest rate, oil prices ($), USD/BRL, GDP, GNI, 
-#   govt spending, CPI, phillips curve?
-# Use data other than raw data without transformation
+#   inflation,
+#   unemployment, 
+#   interest rate,
+#   oil prices ($),*
+#   USD/BRL,
+#   GDP,
+#   govt spending,
+#   CPI
