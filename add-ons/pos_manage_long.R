d41ada3ff208745a742776f9c2429f765659ed5b
Cássio Jandir Pagnoncelli <cassiopagnoncelli@gmail.com>
2015-10-13 15:34:20 -0300

    change pos manage long

--- add-ons/pos_manage_long.R
+++ add-ons/pos_manage_long.R
@@ -2,10 +2,17 @@
 pos_manage_long <- function() {
   s <- as.vector(all_series[open_positions$epoch[1]:epoch,open_positions$instrument_id])
   s <- s / s[1] - 1
+  
   n <- length(s)
   r <- s[n]
   
-  if (r > 1 || r < -0.1)
+  if (n < 10)
+    return(0)
+  
+  if (r > 1)
+    return(1)
+  
+  if (r < -0.1)
     return(1)
   
   0
